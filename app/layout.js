1e37a36bffcb2f83214217020b158d3e45a36356
rovor-debug <gp.rovor@gmail.com>
2026-08-04 20:13:51 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,23 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
+export const metadata = {
+  title: "ROVOR Architecture Studio",
+  description:
+    "ROVOR is an architecture studio focused on design, concept development, competitions and remote collaboration.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+
+        {children}
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
