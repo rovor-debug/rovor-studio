255548f7dfb11a9f31443e8aa32a91f295668a1b
rovor-debug <gp.rovor@gmail.com>
2026-08-04 20:15:08 +0330

    Update competitions.js

--- app/data/competitions.js
+++ app/data/competitions.js
@@ -1,1 +1,20 @@
+const competitions = [
+  {
+    name: "Warming Huts",
+    organizer: "International Design Competition",
+    year: "2026",
+    category: "Architectural Competition",
+    result: "Entry",
+    team: "ROVOR Studio",
+  },
+  {
+    name: "Future Cultural Space",
+    organizer: "Architecture Competition",
+    year: "2026",
+    category: "Cultural Design",
+    result: "Shortlisted",
+    team: "ROVOR Studio",
+  },
+];
 
+export default competitions;
