598d528864eaab74da378813c8e06483852ee3fe
rovor-debug <gp.rovor@gmail.com>
2026-08-04 20:01:22 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,23 +1,29 @@
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <section className="min-h-screen flex items-center justify-center bg-white px-6">
-        <div className="max-w-5xl text-center">
-          <h1 className="text-6xl md:text-8xl font-light tracking-widest">
-            ROVOR
-          </h1>
+    <>
+      <Navbar />
 
-          <p className="mt-8 text-lg md:text-xl text-gray-600">
-            Architecture Studio
-          </p>
+      <main>
+        <section className="min-h-screen flex items-center justify-center bg-white px-6">
+          <div className="max-w-5xl text-center">
 
-          <p className="mt-4 max-w-2xl mx-auto text-gray-500">
-            Designing spaces with clarity, creativity, precision and
-            responsibility.
-          </p>
-        </div>
-      </section>
-    </main>
+            <h1 className="text-6xl md:text-8xl font-light tracking-widest">
+              ROVOR
+            </h1>
+
+            <p className="mt-8 text-lg md:text-xl text-gray-600">
+              Architecture Studio
+            </p>
+
+            <p className="mt-4 max-w-2xl mx-auto text-gray-500">
+              Designing spaces with clarity, creativity, precision and responsibility.
+            </p>
+
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
