00bc288cf3cb62a3c31bba3701af168473e2bd68
rovor-debug <gp.rovor@gmail.com>
2026-08-04 20:02:45 +0330

    Update projects.js

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,1 +1,31 @@
+const projects = [
+  {
+    title: "Minimal House",
+    location: "Tehran, Iran",
+    year: "2026",
+    type: "Residential",
+    status: "Concept",
+    area: "250 m²",
+    image: "/images/projects/project-01.jpg",
+  },
+  {
+    title: "Cultural Space",
+    location: "Berlin, Germany",
+    year: "2026",
+    type: "Cultural",
+    status: "Competition Entry",
+    area: "1200 m²",
+    image: "/images/projects/project-02.jpg",
+  },
+  {
+    title: "Urban Pavilion",
+    location: "Canada",
+    year: "2025",
+    type: "Competition",
+    status: "Shortlisted",
+    area: "80 m²",
+    image: "/images/projects/project-03.jpg",
+  },
+];
 
+export default projects;
